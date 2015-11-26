e78b678a1b2214076f7df63d040ef472f630582c
GAONNR <gaon0403@gmail.com>
2015-11-26 12:41:06 +0900

    Lab1

--- Lab1/datalab-handout/datalab-handout/bits.c
+++ Lab1/datalab-handout/datalab-handout/bits.c
@@ -1,7 +1,7 @@
 /*
  * CS:APP Data Lab
  *
- * <Ji Junseop, 20150739>
+ * <GAONNR>
  *
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
